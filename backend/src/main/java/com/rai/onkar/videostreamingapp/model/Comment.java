d0d0fcdf362127b37d74a8e2d1e85a1eeca0551d
Onkar <onkar21.rai@gmail.com>
2022-09-17 17:03:03 +0530

    Modified Comment class as an embedded document

--- backend/src/main/java/com/rai/onkar/videostreamingapp/model/Comment.java
+++ backend/src/main/java/com/rai/onkar/videostreamingapp/model/Comment.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(value = "Comment")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
