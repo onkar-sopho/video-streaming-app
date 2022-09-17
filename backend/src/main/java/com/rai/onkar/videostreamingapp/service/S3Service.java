bbbadc6101f4a292f6f633f9bfa073a0cb727d42
Onkar <onkar21.rai@gmail.com>
2022-09-17 22:48:51 +0530

    Implemented file upload to s3 bucket

--- backend/src/main/java/com/rai/onkar/videostreamingapp/service/S3Service.java
+++ backend/src/main/java/com/rai/onkar/videostreamingapp/service/S3Service.java
@@ -1,13 +1,51 @@
 package com.rai.onkar.videostreamingapp.service;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class S3Service implements FileService{
+
+    public static final String BUCKET_NAME = "onkar-project-bucket";
+    private final AmazonS3Client amazonS3Client;
+
     @Override
     public String uploadFile(MultipartFile file) {
         // Upload file to AWS S3
-        return null;
+
+        // Prepare a unique key
+        var filenameExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+        var key = UUID.randomUUID().toString() + filenameExtension;
+
+        // Create metadata
+        var metaData = new ObjectMetadata();
+        metaData.setContentLength(file.getSize());
+        metaData.setContentType(file.getContentType());
+
+        // push the video file to AWS s3 bucket
+        try {
+            amazonS3Client.putObject(BUCKET_NAME, key, file.getInputStream(), metaData);
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "An exception occurred while uploading the file");
+        }
+
+        // add access control list so that this video file can be accessed publicly from our client application without
+        // any authentication
+        amazonS3Client.setObjectAcl(BUCKET_NAME, key, CannedAccessControlList.PublicRead);
+
+        // retrieve the url of the video just pushed to s3 bucket
+        return amazonS3Client.getResourceUrl(BUCKET_NAME, key);
+
     }
 }
