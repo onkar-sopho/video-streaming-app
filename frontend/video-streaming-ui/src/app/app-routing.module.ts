790167bdc557bb7bb98a073c6a9c982842b79766
Onkar <onkar21.rai@gmail.com>
2022-09-24 22:44:59 +0530

    added routing for upload video

--- frontend/video-streaming-ui/src/app/app-routing.module.ts
+++ frontend/video-streaming-ui/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UploadVideoComponent} from "./upload-video/upload-video.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'upload-video', component: UploadVideoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
