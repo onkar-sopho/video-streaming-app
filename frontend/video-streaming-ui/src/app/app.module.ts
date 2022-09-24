f8167092597028d04c28cb00e3cb10c4fde87b35
Onkar <onkar21.rai@gmail.com>
2022-09-24 22:31:44 +0530

    Added ngx file drop module

--- frontend/video-streaming-ui/src/app/app.module.ts
+++ frontend/video-streaming-ui/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UploadVideoComponent } from './upload-video/upload-video.component';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { NgxFileDropModule } from "ngx-file-drop";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadVideoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgxFileDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
