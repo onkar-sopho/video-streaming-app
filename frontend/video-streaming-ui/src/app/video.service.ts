5061706faa2cc696033b8af71ee4caad7e3c6d42
Onkar <onkar21.rai@gmail.com>
2022-10-05 23:02:10 +0530

    Update video.service.ts
    added video service

--- frontend/video-streaming-ui/src/app/video.service.ts
+++ frontend/video-streaming-ui/src/app/video.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,11 @@ export class VideoService {
 
   constructor(private httpClient: HttpClient) { }
 
-  uploadVideo() {
+  uploadVideo(fileEntry: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', fileEntry, fileEntry.name);
 
+    // http post call to backend api to upload the video
+    return this.httpClient.post("http://localhost:8081/api/videos", formData);
   }
 }
